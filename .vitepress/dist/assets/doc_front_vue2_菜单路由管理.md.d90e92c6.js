a59a2cb3d4e5e5bce53968f33cbdf4c3c09f3842
xxm <xxm1995@outlook.com>
2023-02-23 18:20:57 +0800

    可视化大屏

---
+++ .vitepress/dist/assets/doc_front_vue2_菜单路由管理.md.d90e92c6.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as r}from"./app.d9d96f60.js";const f='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"doc/front/vue2/\u83DC\u5355\u8DEF\u7531\u7BA1\u7406.md"}',a={};function o(_,c,n,s,p,d){return r(),t("div")}var l=e(a,[["render",o]]);export{f as __pageData,l as default};
